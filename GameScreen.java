a51044012d4dc603c06a34aa95b07dd197eebb72
TomClancy-py <tomclancy5544@gmail.com>
2020-11-25 07:09:37 +0000

    Modified

--- GameScreen.java
+++ GameScreen.java
@@ -16,14 +16,41 @@ public class GameScreen implements Screen{
 	//world parameters	
 	private final int GAME_WIDTH = 72;
 	private final int GAME_HEIGHT = 128;
+
+	GameScreen(){
+
+		camera = new OrthographicCamera();
+		viewport = new StretchViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
+		background = new Texture("name of the image in the assests folder");
+		backgroundOffset = 0;
+		batch = new SpriteBatch();
+	}
 	
 	@Override
-	public void render(float p1){
+	public void render(float deltaTime){
+		batch.begin();
+
+		//scrollin background
+		backgroundOffset ++;
+		if (backgroundOffset % WORLD_HEIGHT == 0){
+			backgroundOffset = 0;
+			
+		}
 		
+		
+		batch.draw(background, 0, -backgroundOffset, WORLD_WIDTH, WORLD_HEIGHT);
+		batch.draw(background, 0, -backgroundOffset + WORLD_HEIGHT, WORLD_WIDTH, WORLD_HEIGHT);
+		
+
+
+
+		batch.end();
 	}
 
 	@Override
 	public void resize(int p1, int p2){
+		viewport.update(width, height,centerCamera true);
+		batch.setProjectMatrix(camera.combined);
 		
 	}
 
