957c63f2067b9b874d22ce3bb71a534160fbe37c
TomClancy-py <tomclancy5544@gmail.com>
2020-11-25 03:33:02 +0000

    Game screen class

---
+++ GameScreen.java
@@ -0,0 +1,54 @@
+package com.guddu.game.screen;
+
+import com.badlogic.gdx.Screen;
+
+public class GameScreen implements Screen{
+	//Screen
+	private Camera camera;
+	private ViewPort viewport;
+	//graphics 
+	private SpriteBatch batch;
+	private Texture background;
+	
+	//timing
+	private int backgroundOffset;
+
+	//world parameters	
+	private final int GAME_WIDTH = 72;
+	private final int GAME_HEIGHT = 128;
+	
+	@Override
+	public void render(float p1){
+		
+	}
+
+	@Override
+	public void resize(int p1, int p2){
+		
+	}
+
+	@Override 
+	public void show(){
+		
+	}
+	@Override 
+	public void hide(){
+			
+	}
+	@Override 
+	public void pause(){
+				
+	}
+
+	@Override 
+	public void resume(){
+					
+	}
+
+	@Override
+	public void dispose(){
+		
+	}
+		
+
+}
